6e8326331028d5774e6e1abd20105e65a8fead9f
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-15 10:14:40 +0530

    Same function with frequenct Counter

---
+++ addToN.js
@@ -0,0 +1,12 @@
+const addToN=n=>{
+    let total = 0;
+    for(let i=1;i<=n;i++){
+        total+=i;
+    }
+    return total;
+}
+// let t1=performance.now();
+const a=addToN(1000000000)
+// let t2=performance.now();
+// console.log(`Time Elapsed = ${(t2-t1)/1000} seaconds
+console.log(a);
---
+++ same.js
@@ -0,0 +1,41 @@
+const same1=(arr1,arr2)=>{
+    if(arr1.length !==arr2.length){
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i]**2);
+        if(correctIndex===-1){
+            return false;
+        }
+        arr2.splice(correctIndex,1)
+        
+    }
+    return true;
+}
+
+//By Frequency Counter
+const same2=(arr1,arr2)=>{
+    if(arr1.length!==arr2.length){
+        return false;
+    }
+    let frequencyCounter1 ={};
+    let frequencyCounter2 ={};
+    for (const val of arr1) {
+        frequencyCounter1[val]=(frequencyCounter1[val]||0 )+1;
+    }
+    for (const val of arr2) {
+        frequencyCounter2[val]=(frequencyCounter2[val]||0 )+1;
+    }
+    for (let key in frequencyCounter1){
+        if(!(key**2 in frequencyCounter2)){
+            return false;
+        }
+        if(frequencyCounter2[key**2]!==frequencyCounter1[key]){
+            return false;
+        }
+    }
+    return true;
+}
+
+let a =same2([1,2,3],[1,4]);
+console.log(a);
